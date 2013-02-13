f7cdd12005c268484959d8c7343a7590905dccbb
Tom Viner <tom.viner@u-dox.com>
2013-02-13 15:42:20 +0000

    p1: add doctest

--- problem-1.py
+++ problem-1.py
@@ -3,4 +3,11 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
-print sum(x for x in xrange(1000) if not (x%3 and x%5))
+def find_sum(n):
+    """
+    >>> find_sum(10)
+    23
+    """
+    return sum(x for x in xrange(n) if not (x%3 and x%5))
+
+print find_sum(1000)
