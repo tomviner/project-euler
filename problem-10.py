bd6499081b5a8fbdee4f84ea768f79b648513ee1
Tom Viner <tom.viner@u-dox.com>
2013-02-19 18:00:37 +0000

    p10: solved with a sum

---
+++ problem-10.py
@@ -0,0 +1,15 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+from common import *
+
+def sum_of_primes(n):
+    """
+    >>> sum_of_primes(10)
+    17
+    """
+    return sum(primes(max_val=n))
+
+print sum_of_primes(2000000)
