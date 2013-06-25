fabdd10e93da79960eaecaebb722bef2298d95c6
Tom V <tom@viner.tv>
2013-06-25 15:29:03 +0200

    comment update

--- sieve.py
+++ sieve.py
@@ -1,10 +1,14 @@
 import itertools
 
-def s(ns):
+def sieve(ns):
+    """
+    Sieve of eratosthenes
+    (Work in progress, currently just returns odd numbers)
+    """
     n = list(itertools.islice(ns, 1))[0]
     yield n
     for item in itertools.ifilter(lambda x: x % n != 0, ns):
         yield item
 
-ps = s(itertools.count(2))
+ps = sieve(itertools.count(2))
 print list(itertools.islice(ps, 100))
